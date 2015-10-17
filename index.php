d03b7f6251e2d7763d13624825ba03b84c716f8d
aengelke <aengelke@users.noreply.github.com>
2015-10-17 16:08:18 +0200

    Basic login working

--- index.php
+++ index.php
@@ -5,20 +5,18 @@ session_start();
 require_once "database.php";
 require_once "login.php";
 
-$getUsersForName = function($email, $verified = 1) {
-    return Database::queryWith("SELECT userid,email,isEmployee,credentials FROM users WHERE isVerified = $verified AND (email = :email)", array("email" => $email));
-};
-
 $login = new Login(600); // Auto logout after ten minutes inactivity
 
 
-// first char: _ means ordinary, ! means error
+// first char: _ means public, ! means error, u means user (login privileges 1), e means employee (p 2)
 $page = "_home";
 
 if (isset($_GET["page"])) {
     $page = "_" . $_GET["page"];
+    if (strlen($page) > 2 && (substr($page, 1, 1) === "u" || substr($page, 1, 1) === "2")) {
+        $page = $_GET["page"];
+    }
 }
-
 
 // TODO: Some style, html wrapper, etc.
 ?>
@@ -27,6 +25,12 @@ if (isset($_GET["page"])) {
 
 if (!Database::open()) {
     $page = "!dberror";
+}
+
+if (substr($page, 1, 1) === "u" && $login() !== 1) {
+    $page = "!auth";
+} else if (substr($page, 1, 1) === "e" && $login() !== 2) {
+    $page = "!auth";
 }
 
 switch ($page) {
@@ -39,12 +43,17 @@ switch ($page) {
         <form action="?page=doregister" method="post">E-Mail<br><input type="email" name="email"><br><br>Password<br><input type="password" name="password"><br><br><input type="submit" value="Register"></form>
         <?php
         break;
+    case "_login":
+        ?><h1>Login at the SecureBank</h1>
+        <form action="?page=dologin" method="post">E-Mail<br><input type="email" name="email"><br><br>Password<br><input type="password" name="password"><br><br><input type="submit" value="Login"></form>
+        <?php
+        break;
     case "_doregister":
         if (!isset($_POST["email"]) || !isset($_POST["password"])) {
             echo "<h1>Registration failed.</h1>";
         }
         else {
-            $users = $getUsersForName($_POST["email"], 0);
+            $users = Database::queryWith("SELECT userid,email,isEmployee,credentials FROM users WHERE (email = :email)", array("email" => $email));
             if ($users->rowCount() !== 0) {
                 // We don't want to give more details here, do we?
                 echo "<h1>Registration failed.</h1>";
@@ -56,12 +65,45 @@ switch ($page) {
             }
         }
         break;
+    case "_dologin":
+        $getUsersForName = function($email) {
+            return Database::queryWith("SELECT userid,email,isEmployee,credentials FROM users WHERE isVerified = 1 AND (email = :email)", array("email" => $email));
+        };
+        $success = $login->login($_POST, $getUsersForName);
+        if ($success !== 0) {
+            echo "<h1>Login failed.$success</h1>";
+            echo "<a href='?page=login'>Try again</a>";
+        }
+        else {
+            echo "<h1>Login successful.</h1>Redirecting...";
+            if ($login() === 1) header("Location: index.php?page=uhome");
+            if ($login() === 2) header("Location: index.php?page=ehome");
+        }
+        break;
+    case "uhome":
+        echo "<h1>Welcome client</h1>";
+        break;
+    case "ehome":
+        echo "<h1>Welcome employee</h1>";
+        break;
+    case "_logout":
+        $login->reset();
+        header("Location: index.php");
+        break;
     case "!dberror":
         echo "<h1>Could not connect to the database</h1>";
+        break;
+    case "!auth":
+        echo "<h1>You are not authenticated to do this.</h1>";
+        echo "<a href='?page=login'>Login</a>";
         break;
     default:
         echo "<h1>Could not find requested page.</h1><a href='?page=home'>Go home instead</a>";
         break;
+}
+
+if ($login() !== 0) {
+    echo "<p><a href='?page=logout'>Logout</a></p>";
 }
 
 Database::close();

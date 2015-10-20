015629053b5164a179730b92a9f1b42c55e50497
Ralph Oliver Schaumann <absolute512>
2015-10-20 10:42:00 +0200

    'name' parameter validation implementation

--- parser/main.c
+++ parser/main.c
@@ -8,6 +8,8 @@
 
 #include "main.h"
 
+#define DEBUG
+
 // #define DEBUG 1
 
 void process(char * buffer, size_t size){
@@ -16,6 +18,7 @@ void process(char * buffer, size_t size){
     char * iban_val = NULL;
     char * bic_val = NULL;
     char * amount_val = NULL;
+    char * name_val = NULL;
     char * subject_val = NULL;
     char * tan_val = NULL;
     char * date_val = NULL;
@@ -24,150 +27,150 @@ void process(char * buffer, size_t size){
     for(i = 0; i < size; ++i){
         if(buffer[i]=='I'){
             if(!cmp_string(buffer,iban,i,SIZE_4)){
-                #ifdef DEBUG
+#ifdef DEBUG
                 printf("ERROR: invalid parameter in file at position %i (IBAN)",i);
-                #endif
+#endif
                 exit(2);
-                return;
             }
             i += SIZE_4;
             i = parse_value(buffer,&iban_val,i);
         }
+        else if(buffer[i]=='N'){
+            if(!cmp_string(buffer,name,i,SIZE_4)){
+                printf("ERROR: invalid parameter in file at position %i (NAME)",i);
+                return;
+            }
+            i += SIZE_4;
+            i = parse_value(buffer,&name_val,i);
+        }
         else if(buffer[i]=='B'){
             if(!cmp_string(buffer,bic,i,SIZE_3)){
-                #ifdef DEBUG
+#ifdef DEBUG
                 printf("ERROR: invalid parameter in file at position %i (BIC)",i);
-                #endif
+#endif
                 exit(2);
-                return;
             }
             i += SIZE_3;
             i = parse_value(buffer,&bic_val,i);
         }
         else if(buffer[i]=='T'){
             if(!cmp_string(buffer,tan,i,SIZE_3)){
-                #ifdef DEBUG
+#ifdef DEBUG
                 printf("ERROR: invalid parameter in file at position %i (TAN)",i);
-                #endif
+#endif
                 exit(2);
-                return;
             }
             i += SIZE_3;
             i = parse_value(buffer,&tan_val,i);
         }
         else if(buffer[i]=='A'){
             if(!cmp_string(buffer,amount,i,SIZE_6)){
-                #ifdef DEBUG
+#ifdef DEBUG
                 printf("ERROR: invalid parameter in file at position %i (AMOUNT)",i);
-                #endif
+#endif
                 exit(2);
-                return;
             }
             i += SIZE_6;
             i = parse_value(buffer,&amount_val,i);
         }
         else if(buffer[i]=='D'){
             if(!cmp_string(buffer,date,i,SIZE_4)){
-                #ifdef DEBUG
+#ifdef DEBUG
                 printf("ERROR: invalid parameter in file at position %i (DATE)",i);
-                #endif
+#endif
                 exit(2);
-                return;
             }
             i += SIZE_4;
             i = parse_value(buffer,&date_val,i);
         }
         else if(buffer[i]=='S'){
             if(!cmp_string(buffer,subject,i,SIZE_7)){
-                #ifdef DEBUG
+#ifdef DEBUG
                 printf("ERROR: invalid parameter in file at position %i (SUBJECT)",i);
-                #endif
+#endif
                 exit(2);
-                return;
             }
             i += SIZE_7;
             i = parse_value(buffer,&subject_val,i);
         }
         else if(buffer[i]=='C'){
             if(!cmp_string(buffer,comment,i,SIZE_7)){
-                #ifdef DEBUG
+#ifdef DEBUG
                 printf("ERROR: invalid parameter in file at position %i",i);
-                #endif
+#endif
                 exit(2);
-                return;
             }
             i += SIZE_7;
             i = parse_value(buffer,&comment_val,i);
         }
         else {
-            #ifdef DEBUG
+#ifdef DEBUG
             printf("ERROR: invalid parameter in file at position %i",i);
-            #endif
+#endif
             exit(2);
-            return;
         }
         if(++i == -1){
-            #ifdef DEBUG
+#ifdef DEBUG
             printf("ERROR: illegal character ';' %c",0);
-            #endif
+#endif
             exit(2);
         }
     }
 
     //validate input
     if(!validate_iban(iban_val)){
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("ERROR: invalid IBAN:\n'%s'",iban_val);
-        #endif
+#endif
         exit(2);
-        return;
     }
     if(!validate_bic(bic_val)){
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("ERROR: invalid BIC:\n'%s'",bic_val);
-        #endif
+#endif
         exit(2);
-        return;
+    }
+    if(!validate_name(name_val)){
+#ifdef DEBUG
+        printf("ERROR: invalid name:\n'%s'",name_val);
+#endif
+        exit(2);
     }
     if(!validate_tan(tan_val)){
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("ERROR: invalid TAN:\n'%s'",tan_val);
-        #endif
+#endif
         exit(2);
-        return;
     }
     if(!validate_amount(amount_val)){
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("ERROR: invalid amount:\n'%s'",amount_val);
-        #endif
+#endif
         exit(2);
-        return;
     }
     if(!validate_date(date_val)){
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("ERROR: invalid date:\n'%s'",date_val);
-        #endif
+#endif
         exit(2);
-        return;
     }
     if(!validate_subject(subject_val)){
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("ERROR: invalid subject:\n'%s'",subject_val);
-        #endif
+#endif
         exit(2);
-        return;
     }
     if(!validate_comment(comment_val)){
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("ERROR: invalid comment:\n'%s'",comment_val);
-        #endif
+#endif
         exit(2);
-        return;
     }
 
     //print out provided values
     print_value(iban_val);
     print_value(bic_val);
+    print_value(name_val);
     print_value(tan_val);
     print_value(amount_val);
     print_value(date_val);
@@ -185,11 +188,10 @@ int cmp_string(char * actual, const char * expected, int pos, const int size){
 
 int parse_value(char * buffer, char ** target, int index){
     if(buffer[index++] != '=' || buffer[index++] != '"'){
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("ERROR: invalid format");
-        #endif
+#endif
         exit(2);
-        return -1;
     }
     target[0] = (buffer + index);
     while(buffer[index] != '"'){
@@ -204,9 +206,9 @@ int validate_iban(char *val){
     int length = -1;
     int cur;
     while((cur = val[++length])){
-        if(!(cur >= '0' && cur < ':') &&
-           !(cur >= 'a' && cur < 'z') &&
-           !(cur >= 'A' && cur < 'Z'))
+        if(!(cur >= '0' && cur <= '9') &&
+           !(cur >= 'a' && cur <= 'z') &&
+           !(cur >= 'A' && cur <= 'Z'))
             return 0;
     }
     return length < 35;
@@ -216,21 +218,36 @@ int validate_bic(char * val){
     int length = -1;
     int cur;
     while((cur = val[++length])){
-        if(!(cur >= '0' && cur < ':') &&
-           //!(cur >= 'a' && cur < 'z') &&
-           !(cur >= 'A' && cur < 'Z'))
+        if(!(cur >= '0' && cur <= '9') &&
+           //!(cur >= 'a' && cur <= 'z') &&
+           !(cur >= 'A' && cur <= 'Z'))
             return 0;
     }
     return length >= 8 && length < 13;
+}
+
+int validate_name(char * val){
+    int length = -1;
+    int cur;
+    while((cur = val[++length])){
+        if(!(cur >= '0' && cur <= '9') &&
+           !(cur >= 'a' && cur <= 'z') &&
+           !(cur >= 'A' && cur <= 'Z') &&
+           cur != ',' && cur != '-' && cur != ' ') {
+            printf("'%c'\n",cur);
+            return 0;
+        }
+    }
+    return length <= 160;
 }
 
 int validate_tan(char * val){
     int length = -1;
     int cur;
     while((cur = val[++length])){
-        if(!(cur >= '0' && cur < ':') &&
-           //!(cur >= 'a' && cur < 'z') &&
-           !(cur >= 'A' && cur < 'Z'))
+        if(!(cur >= '0' && cur <= '9') &&
+           //!(cur >= 'a' && cur <= 'z') &&
+           !(cur >= 'A' && cur <= 'Z'))
             return 0;
     }
     return length != 15;
@@ -241,10 +258,9 @@ int validate_amount(char * val){
     int length = -1;
     int cur;
     while((cur = val[++length])){
-        if(!(cur >= '0' && cur < ':') &&
-           //!(cur >= 'a' && cur < 'z') &&
-           !(cur >= 'A' && cur < 'Z') &&
-                cur != ',' && cur != '.')
+        if(!(cur >= '0' && cur <= ':') &&
+           !(cur >= 'A' && cur <= 'Z') &&
+           cur != ',' && cur != '.')
             return 0;
     }
     return 1;
@@ -318,23 +334,23 @@ int main(int argc, char * argv[]){
 
     //check parameters
     if(argc != 2){
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("ERROR: no filename supplied");
-        #endif
+#endif
         exit(2);
     }
     //check file exists
     if( access(argv[1],F_OK) == -1){
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("ERROR: file '%s' does not exist",argv[1]);
-        #endif
+#endif
         exit(2);
     }
     //check file readable
     if( access(argv[1],R_OK) == -1){
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("ERROR: file '%s' is not readable",argv[1]);
-        #endif
+#endif
         exit(2);
     }
     //open file
